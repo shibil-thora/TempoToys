5b8ae683b0174aae329517ce50b21d7536795bd1
shibil-thora <shibilallakkat123@gmail.com>
2024-01-26 18:56:38 +0530

    The database changed

--- TempoTees/TempoTees/settings.py
+++ TempoTees/TempoTees/settings.py
@@ -84,12 +84,8 @@ WSGI_APPLICATION = 'TempoTees.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'tempotees',
-        'USER': 'postgres',
-        'PASSWORD': '8912',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
