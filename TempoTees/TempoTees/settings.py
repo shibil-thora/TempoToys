0e2e243ffac34678fe17802066de872c685215ac
shibil-thora <shibilallakkat123@gmail.com>
2024-01-27 07:23:24 +0530

    allow host change

--- TempoTees/TempoTees/settings.py
+++ TempoTees/TempoTees/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-_4v6cv^o4@jbqfxp9xelytplbvi&m$a@=g-h6z3btm1vb-zyy5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.31.12.253']
+ALLOWED_HOSTS = ['172.31.12.253', 'ec2-65-0-99-30.ap-south-1.compute.amazonaws.com']
 
 
 # Application definition
