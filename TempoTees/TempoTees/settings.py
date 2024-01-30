734264610d03fed1c7d8501b1ecfe23366e24e1f
shibil-thora <shibilallakkat123@gmail.com>
2024-01-30 20:46:10 +0530

    the path change by samad

--- TempoTees/TempoTees/settings.py
+++ TempoTees/TempoTees/settings.py
@@ -126,13 +126,14 @@ USE_TZ = True
 
 # the additional settings added by me will be here -------------------------------
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 # TEMPLATE_DEBUG = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/home/ubuntu/TempoToys/TempoTees/staticfiles/'
+
 
 
 LOGIN_REDIRECT_URL = 'h:home'
